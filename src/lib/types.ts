f3efe0aabf2c113b8d09837729c591ad3bf487cc
Parth Gaglani <parthgaglani27@gmail.com>
2025-02-09 03:30:18 +0530

    purge pt 2

--- src/lib/types.ts
+++
@@ -1,9 +0,0 @@
-export type UserRole = 'employee' | 'finance' | 'manager';
-
-export interface UserData {
-  uid: string;
-  email: string;
-  role: UserRole;
-  displayName?: string;
-  createdAt: number;
-} 
